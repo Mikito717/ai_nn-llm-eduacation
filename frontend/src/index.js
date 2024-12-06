5a3ddc78082f0c15f387e0370a5eb138f2363f9a
Mikito717 <kurahara0717@akane.waseda.jp>
2024-12-07 00:49:49 +0900

    .env

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 // Code: Reactのエントリーポイント
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-const container = document.getElementById('root');
-const root = createRoot(container); // createRootコンテナを作成
-root.render(<App />); // Appコンポーネントをレンダリング
+const container = document.getElementById('root')
+const root = createRoot(container) // createRootコンテナを作成
+root.render(<App />) // Appコンポーネントをレンダリング
