3b7839fd7b82a511fbccaef23daa4483582d7232
wao <kurahara0717@akane.waseda.jp>
2024-12-10 12:01:22 +0900

    Untracked files:

--- frontend/src/components/Game.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Game = () => {
-  return <div id="phaser-game"></div>
-}
-
-export default Game
--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-// Code: Reactのエントリーポイント
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-const container = document.getElementById('root')
-const root = createRoot(container) // createRootコンテナを作成
-root.render(<App />) // Appコンポーネントをレンダリング
